3b9dc161a66ec8149d5ff2653cd548a7053e639a
KaterynaTkachenko <katerina.timofeevna@gmail.com>
2022-12-16 16:41:51 -0500

    fix

--- index.php
+++ index.php
@@ -1,13 +1,1 @@
 <?php
-
-function mySqrt($x) {
-    for($i = 0; $i < PHP_INT_MAX; $i++) {
-        if ($i * $i == $x) {
-            return $i;
-        } else if ($i * $i > $x) {
-            return $i-1;
-        }
-    }
-}
-
-echo mySqrt('8');
