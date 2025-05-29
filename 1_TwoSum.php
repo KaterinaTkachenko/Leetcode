aee88abf889d0334f14b85712dd8a64f9f311360
KaterynaTkachenko <katerina.timofeevna@gmail.com>
2025-05-29 15:50:18 -0400

    two sum

--- 1_TwoSum.php
+++ 1_TwoSum.php
@@ -1,15 +1,36 @@
 <?php
-function twoSum($nums, $target) {
-    $current = 0;
-    $result = [];
-    for($i=0; $i<count($nums); $i++){
-        for($j=$current; $j<count($nums); $j++){
-            $sum = $nums[$current]+$nums[$i+1];
-            if($sum === $target){
-                $result = [$current, $i+1];
-            }
+$nums = [2, 7, 8];
+$target = 9;
+$res = [];
+$map = [];
+function twoSum1($nums, $target) {
+    for($i = 0; $i < count($nums); $i++) {
+        $x = $target - $nums[$i];
+        if (in_array($x, $nums)) {
+            $x_key = array_search($x, $nums);
+            if ($x_key !== $i)
+                $res = [$x_key, $i];
         }
-        $current++;
     }
-    return $result;
+    return $res;
+}
+
+function twoSum2($nums, $target) {
+    $map = [];
+    for($i = 0; $i < count($nums); $i++) {
+        for ($j = $i+1; $j < count($nums); $j++){
+            if($nums[$i] + $nums[$j] === $target)
+                $res = [$i, $j];
+        }
+    }
+    return $res;
+}
+
+
+function twoSum3($nums, $target) {
+    $diff = $target - $nums[$i];
+    if(array_key_exists($diff, $map)){
+        print_r([$map[$diff], $i]);
+    }
+    $map[$nums[$i]] = $i;
 }
--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php
+$map = [];
+$nums = [2, 7, 8];
+$target = 10;
+
+for ($i = 0; $i < count($nums); $i++){
+    $diff = $target - $nums[$i];
+    if(array_key_exists($diff, $map)){
+        print_r([$map[$diff], $i]);
+    }
+    $map[$nums[$i]] = $i;
+}
